f42fbb6235b576d6db198f3dbfd656306c37a9ec
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-07-21 01:45:04 +0100

    file syze limiting

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 var Hapi = require('hapi');
 var fs   = require('fs');
 
-var server = new Hapi.Server('localhost', 8768, { cors: true });
+var server = new Hapi.Server('localhost', 8767, { cors: true });
 
 server.route({
  		method: 'POST',
    	path: '/upload',
    	config: {
     	payload:{
-        maxBytes: 209715200,
+        maxBytes: 3072,
         output:'stream',
         parse: true
       }
